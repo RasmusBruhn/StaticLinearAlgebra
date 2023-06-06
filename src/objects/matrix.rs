6fa5fd23a493456e0aa927ecf7f82d41c58b988a
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2023-06-06 11:48:39 +0200

    Added addition

--- src/objects/matrix.rs
+++ src/objects/matrix.rs
@@ -1,4 +1,4 @@
-use std::ops::{Index, IndexMut};
+use std::ops::{Index, IndexMut, Add};
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Matrix<T, const R: usize, const C: usize>
@@ -64,20 +64,53 @@ where
     }
 }
 
-/*
-impl<T> std::ops::Add for Matrix<T>
+impl<T, TR, O, const R: usize, const C: usize> Add<Matrix<TR, R, C>> for Matrix<T, R, C>
 where
-    T: Clone,
-    T: std::ops::Add<Output = T>,
-    T: std::ops::Mul<Output = T>,
-    T: std::iter::Sum,
+    T: Copy,
+    T: std::ops::Add<TR, Output = O>,
+    TR: Copy,
+    O: Copy,
 {
-    type Output = Matrix<T>;
+    type Output = Matrix<O, R, C>;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        operators::add::matrix(self, rhs).unwrap()
+    fn add(self, rhs: Matrix<TR, R, C>) -> Self::Output {
+        let values: [[O; C]; R] = match self.values.iter().zip(rhs.values.iter())
+        .map(|(lhs, rhs)| match lhs.iter().zip(rhs.iter())
+        .map(|(lhs, rhs)| *lhs + *rhs).collect::<Vec<O>>().try_into() {
+            Ok(result) => result,
+            Err(_) => panic!("Should not happen"),
+        }).collect::<Vec<[O; C]>>().try_into() {
+            Ok(result) => result,
+            Err(_) => panic!("Should not happen"),
+        };
+
+        Self::Output {values}
     }
-} */
+}
+
+impl<T, TR, O, const R: usize, const C: usize> Add<&Matrix<TR, R, C>> for &Matrix<T, R, C>
+where
+    T: Copy,
+    for<'a> &'a T: std::ops::Add<&'a TR, Output = O>,
+    TR: Copy,
+    O: Copy,
+{
+    type Output = Matrix<O, R, C>;
+
+    fn add(self, rhs: &Matrix<TR, R, C>) -> Self::Output {
+        let values: [[O; C]; R] = match self.values.iter().zip(rhs.values.iter())
+        .map(|(lhs, rhs)| match lhs.iter().zip(rhs.iter())
+        .map(|(lhs, rhs)| lhs + rhs).collect::<Vec<O>>().try_into() {
+            Ok(result) => result,
+            Err(_) => panic!("Should not happen"),
+        }).collect::<Vec<[O; C]>>().try_into() {
+            Ok(result) => result,
+            Err(_) => panic!("Should not happen"),
+        };
+
+        Self::Output {values}
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -135,6 +168,22 @@ mod tests {
             let mut result = Matrix::new(&[[0, 1, 2], [3, 4, 5]]);
             result[0][2] = 10;
             assert_eq!(10, result[0][2]);
+        }
+
+        #[test]
+        fn add() {
+            let a = Matrix::new(&[[0, 1], [2, 3]]);
+            let b = Matrix::new(&[[0, 10], [20, 30]]);
+            let c = a + b;
+            assert_eq!([[0, 11], [22, 33]], c.values);
+        }
+
+        #[test]
+        fn add_ref() {
+            let a = Matrix::new(&[[0, 1], [2, 3]]);
+            let b = Matrix::new(&[[0, 10], [20, 30]]);
+            let c = &a + &b;
+            assert_eq!([[0, 11], [22, 33]], c.values);
         }
     }
 }
